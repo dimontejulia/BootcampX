143e40fb727728a06e5e2914fe747ca0a4c9a53c
Julia Di Monte <dimontejulia@gmail.com>
2020-11-23 20:07:45 +0000

    join practice

---
+++ 1_queries/7_rollover_students.sql
@@ -0,0 +1,7 @@
+-- rollover student:  start_date will differ from their cohort's start_date.
+
+SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
